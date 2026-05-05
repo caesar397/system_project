a77a8df4636755b8d2add82e2b9d91d073a8d456
caesar397 <korneluk.gabriela@gmail.com>
2026-05-05 22:12:43 +0200

    feat: utworzenie obiektu wydarzenie

--- models.py
+++ models.py
@@ -8,11 +8,18 @@ class Firmy(Protocol):
         self.lon = lon
 
 class Pracownicy(Protocol):
-    def __init__(self, id, imie, nazwisko, lat, lon, id_firmy):
+    def __init__(self, id, imie, nazwisko, firma):
         self.id = id
         self.imie = imie
         self.nazwisk = nazwisko
+        self.firma = firma
+
+class Wydarzenia(Protocol):
+    def __init__(self, id, nazwa, lat, lon, firma):
+        self.id = id
+        self.nazwa = nazwa
         self.lat = lat
         self.lon = lon
-        self.id_firmy = id_firmy
+        self.firma = firma
+
 
